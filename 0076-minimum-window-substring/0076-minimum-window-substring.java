e08a8e595fec07163020456bf8de91a622d60952
Ananyarajmathur <142478332+Ananyarajmathur@users.noreply.github.com>
2026-09-08 00:00:24 +0530

    Time: 2 ms (99.76%), Space: 44.9 MB (99.56%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,60 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        int[] freq = new int[128];
+
+        // Store required character frequencies
+        for (char c : t.toCharArray()) {
+            freq[c]++;
+        }
+
+        int left = 0;
+        int required = t.length();
+
+        int minLength = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+
+            // If this character was still needed
+            if (freq[c] > 0) {
+                required--;
+            }
+
+            freq[c]--;
+
+            // Window is valid
+            while (required == 0) {
+
+                // Update minimum window
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    start = left;
+                }
+
+                // Remove left character
+                char leftChar = s.charAt(left);
+
+                freq[leftChar]++;
+
+                if (freq[leftChar] > 0) {
+                    required++;
+                }
+
+                left++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            return "";
+        }
+
+        return s.substring(start, start + minLength);
+    }
+}
